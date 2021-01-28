edbf3853fa5926b42e36fbdfae2979f4a8d0d626
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-28 18:15:08 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Create and configure Slim app
+$config = ['settings' => [
+    'addContentLengthHeader' => false,
+]];
+$app = new \Slim\App($config);
+
+// Define app routes
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->write("Hello " . $args['name']);
+});
+
+// Run app
+$app->run();
